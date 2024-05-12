2dda03a36818b2fc9df56f9304754ac51a4411d5
Ross A. Reyman <developer@telswitch.com>
2024-05-11 18:33:58 -0700

    refactor: Update Hero section styles and animations in Hero.vue

--- src/sections/Hero.vue
+++ src/sections/Hero.vue
@@ -12,7 +12,7 @@
 		<div class="section-inner">
 			<div class="flex-[2] self-center">
 				<h2
-					class="relative inline-block bg-gradient-to-br from-ts-orange-coral-500 via-ts-blue-spruce-800 to-ts-green-mineral-400 bg-clip-text text-5xl font-extrabold uppercase leading-none text-surface-1 text-transparent drop-shadow-outline max_m_xl:text-3xl"
+					class="relative inline-block bg-gradient-to-r from-ts-orange-coral-400 to-ts-green-mineral-600 bg-clip-text text-5xl font-extrabold uppercase leading-none text-surface-1 text-transparent drop-shadow-outline max_m_xl:text-3xl"
 				>
 					Bringing Insights Into Life’s Digital Fingerprints
 				</h2>
@@ -24,14 +24,14 @@
 					<span>Read More…</span>
 				</button>
 			</div>
-			<div class="flex-1">
-				<svg class="h-full w-full">
+			<div class="fade-in flex-1">
+				<svg class="h-full w-full -translate-x-10 scale-125">
 					<use href="@/assets/telswitch-dude-white.svg#logo" />
 				</svg>
 			</div>
 		</div>
 		<div
-			class="mx-auto mt-8 flex items-start justify-center gap-8 rounded-full bg-gradient-to-br from-ts-green-mineral-600 to-ts-green-mineral-800 p-8 px-24 text-ts-green-mineral-200 drop-shadow-outline"
+			class="fade-in fast mx-auto mt-8 flex items-start justify-center gap-8 rounded-full bg-gradient-to-b from-ts-green-mineral-600 to-ts-green-mineral-800 p-8 px-24 text-ts-green-mineral-200 drop-shadow-outline"
 		>
 			<div class="fade-in flex flex-col items-center justify-center text-lg font-light">
 				<span class="mb-1 text-5xl font-bold text-ts-green-mineral-100 drop-shadow-outline">
@@ -83,19 +83,22 @@
 .fade-in {
 	opacity: 0; /* Start at opacity 0 */
 	animation: fadeIn 2s ease-in-out forwards;
+	&.fast {
+		animation-duration: 0.5s;
+	}
 }
 
 /* Apply different animation delays */
 .fade-in:nth-child(1) {
-	animation-delay: 0.1s;
-}
-.fade-in:nth-child(2) {
 	animation-delay: 0.5s;
 }
-.fade-in:nth-child(3) {
+.fade-in:nth-child(2) {
 	animation-delay: 1s;
 }
-.fade-in:nth-child(4) {
+.fade-in:nth-child(3) {
 	animation-delay: 1.5s;
+}
+.fade-in:nth-child(4) {
+	animation-delay: 2s;
 }
 </style>
